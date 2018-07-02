ae18c34b790a77e94a98b844a26b0bc732dc0e87
Jui-Ting Chien <ydnaandy123@gmail.com>
2018-07-02 09:30:36 +0800

    responsive font size

--- javascript/landing.js
+++ javascript/landing.js
@@ -1,5 +1,21 @@
-$(document).ready(function(){
-  //$('div#cover-content').fadeIn(1500);
-  //$('div#cover-content').slidedown(1500);
-  $('div#cover-content').css("animation-name", "example");
+$(document).ready(function(e) { 
+  // executes when HTML-Document is loaded and DOM is ready  
+  console.log("page is loading now"); 
+});
+
+$(window).on('load', function(e){
+  //when html page complete loaded
+  console.log("completely loaded");   
+  $("#landing-content").addClass("top-to-center-animation").removeClass("ready-to-appear");
+})
+$(window).scroll(function() {
+  $(".ready-to-appear").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 1000) {
+      $(this).addClass("top-to-center-animation").removeClass("ready-to-appear");
+      console.log('hi')
+    }
+  });
 });
