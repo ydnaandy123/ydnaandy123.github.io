aede1816269c804f7eca89b31be59c982b013502
Jui-Ting Chien <ydnaandy123@gmail.com>
2018-07-02 16:32:31 +0800

    navBar

--- javascript/landing.js
+++ javascript/landing.js
@@ -1,21 +1,30 @@
-$(document).ready(function(e) { 
+$(document).ready(function (e) {
   // executes when HTML-Document is loaded and DOM is ready  
-  console.log("page is loading now"); 
+  console.log("page is loading now");
 });
 
-$(window).on('load', function(e){
-  console.log("completely loaded");   
-  if ($(window).scrollTop() < window.innerHeight){
+$(window).on('load', function (e) {
+  console.log("completely loaded");
+  if ($(window).scrollTop() < window.innerHeight) {
     $("#landing-content").addClass("top-to-center-animation").removeClass("ready-to-appear");
-  }  
+  }
 })
-$(window).scroll(function() {
-  $(".ready-to-appear").each(function(){
+$(window).scroll(function () {
+  var winTop = $(window).scrollTop();
+  $(".ready-to-appear").each(function () {
     var pos = $(this).offset().top;
-    var winTop = $(window).scrollTop();
-    if (pos < winTop +  window.innerHeight && pos > winTop) {
+    if (pos < winTop + window.innerHeight && pos > winTop) {
       $(this).addClass("top-to-center-animation").removeClass("ready-to-appear");
       console.log('hi')
     }
   });
+  if (winTop < window.innerHeight / 2){
+    //$("#testt").css({"opacity" : "1.0"})
+    $("#testt").css({"background-color" : "rgba(255, 255, 255, 0.6)"});
+  } else{
+    //$("#testt").css({"opacity" : "0.3"})
+    $("#testt").css({"background-color" : "rgba(227, 242, 253, 0.4)"});
+  }
+  
+  
 });
