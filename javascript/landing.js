777361ce67f9c02707be3354500db70e0b786cbf
Jui-Ting Chien <ydnaandy123@gmail.com>
2018-07-03 13:07:19 +0800

    landing image

--- javascript/landing.js
+++ javascript/landing.js
@@ -5,12 +5,16 @@ $(document).ready(function (e) {
 
 $(window).on('load', function (e) {
   console.log("completely loaded");
+
+  // Check landing animation even no scroll events
   if ($(window).scrollTop() < window.innerHeight) {
     $("#landing-content").addClass("top-to-center-animation").removeClass("ready-to-appear");
   }
 })
+
 $(window).scroll(function () {
   var winTop = $(window).scrollTop();
+  // Check all ready-to-appear elements
   $(".ready-to-appear").each(function () {
     var pos = $(this).offset().top;
     if (pos < winTop + window.innerHeight && pos > winTop) {
@@ -18,13 +22,13 @@ $(window).scroll(function () {
       console.log('hi')
     }
   });
+  // Check navbar
   if (winTop < window.innerHeight / 2){
     //$("#testt").css({"opacity" : "1.0"})
     $("#testt").css({"background-color" : "rgba(255, 255, 255, 0.6)"});
   } else{
     //$("#testt").css({"opacity" : "0.3"})
-    $("#testt").css({"background-color" : "rgba(227, 242, 253, 0.4)"});
-  }
-  
+    $("#testt").css({"background-color" : "rgba(227, 242, 253, 0.8)"});
+  }  
   
 });
