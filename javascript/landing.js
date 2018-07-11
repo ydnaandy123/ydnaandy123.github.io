46bfa7d37bf0e3e680404a539a4979b312e6d616
Jui-Ting Chien <ydnaandy123@gmail.com>
2018-07-11 10:44:54 +0800

    scroll

--- javascript/landing.js
+++ javascript/landing.js
@@ -1,6 +1,45 @@
 $(document).ready(function (e) {
   // executes when HTML-Document is loaded and DOM is ready  
   console.log("page is loading now");
+  $(".navbar a, footer a[href='#landing-wrapper']").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;$('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  $(window).scroll(function () {
+    var winTop = $(window).scrollTop();
+    // Check all ready-to-appear elements
+    $(".ready-to-appear").each(function () {
+      var pos = $(this).offset().top;
+      if (pos < winTop + window.innerHeight && pos > winTop) {
+        $(this).addClass("top-to-center-animation").removeClass("ready-to-appear");
+        console.log('hi')
+      }
+    });
+    $(".ready-to-appear2").each(function () {
+      var pos = $(this).offset().top;
+      if (pos < winTop + window.innerHeight && pos > winTop) {
+        $(this).addClass("down-to-center-animation").removeClass("ready-to-appear2");
+        console.log('hi')
+      }
+    });
+    // Check navbar
+    if (winTop < window.innerHeight / 2) {
+      //$("#testt").css({"opacity" : "1.0"})
+      //$("#testt").css({"background-color" : "rgba(255, 255, 255, 0.6)"});
+    } else {
+      //$("#testt").css({"opacity" : "0.3"})
+      //$("#testt").css({"background-color" : "rgba(227, 242, 253, 0.8)"});
+    }
+  
+  });
+  
 });
 
 $(window).on('load', function (e) {
@@ -11,31 +50,3 @@ $(window).on('load', function (e) {
   //  $("#landing-content").addClass("top-to-center-animation").removeClass("ready-to-appear");
   //}
 })
-
-$(window).scroll(function () {
-  var winTop = $(window).scrollTop();
-  // Check all ready-to-appear elements
-  $(".ready-to-appear").each(function () {
-    var pos = $(this).offset().top;
-    if (pos < winTop + window.innerHeight && pos > winTop) {
-      $(this).addClass("top-to-center-animation").removeClass("ready-to-appear");
-      console.log('hi')
-    }
-  });
-  $(".ready-to-appear2").each(function () {
-    var pos = $(this).offset().top;
-    if (pos < winTop + window.innerHeight && pos > winTop) {
-      $(this).addClass("down-to-center-animation").removeClass("ready-to-appear2");
-      console.log('hi')
-    }
-  });
-  // Check navbar
-  if (winTop < window.innerHeight / 2){
-    //$("#testt").css({"opacity" : "1.0"})
-    //$("#testt").css({"background-color" : "rgba(255, 255, 255, 0.6)"});
-  } else{
-    //$("#testt").css({"opacity" : "0.3"})
-    //$("#testt").css({"background-color" : "rgba(227, 242, 253, 0.8)"});
-  }  
-  
-});
