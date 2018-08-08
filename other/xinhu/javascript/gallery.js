fb9982033e5a608fcbc879bdfe295fc2f2676a9c
Jui-Ting Chien <ydnaandy123@gmail.com>
2018-08-08 11:00:06 +0800

    personal work renovation

--- other/xinhu/javascript/gallery.js
+++ other/xinhu/javascript/gallery.js
@@ -1,77 +1,81 @@
-var col_template, gallery_row, cur_class;
 var gallery_div, gallery_cell_template, gallery_cell_height, cur_class="all";
 
 function filterClass(category_class) {
   cur_class = category_class
   if (cur_class == "all"){
-    gallery_row.children().each(function(index) {
+    gallery_div.children().each(function(index) {
+      console.log(index);
         $(this).css({
             "opacity": "1.0"
         })
         //$(this).show()
-        $(this).removeClass("d-none").addClass("d-block");
-        console.log(index);
     });
   }
   else{
-    gallery_row.children().each(function(index) {
+    gallery_div.children().each(function(index) {
         if ($(this).attr("category_class") != cur_class) {
           $(this).css({
               "opacity": "0.0"
           });
           //$(this).hide()
-          $(this).removeClass("d-block").addClass("d-none");
-          return true;
         }
-        $(this).css({
-            "opacity": "1.0",
-        })
-        //$(this).show()
-        $(this).removeClass("d-none").addClass("d-block");
+        else{
+          $(this).css({
+              "opacity": "1.0",
+          })
+          //$(this).show()
+        }
     });
   }
-  gallery_row.removeClass(gallery_row.attr("cur_class"))
-  gallery_row.attr("cur_class", cur_class)
-  gallery_row.addClass(cur_class)
+  gallery_div.removeClass(gallery_div.attr("cur_class"))
+  gallery_div.attr("cur_class", cur_class)
+  gallery_div.addClass(cur_class)
 }
 $(document).ready(function (e) {
   // Debug
   checkDuplicateID();
   // Get elements after document ready.
-  col_template = $("#video-template > div").clone();
-  gallery_row = $("#gallery_row");
-
   gallery_div = $("#gallery-div2");
   gallery_cell_template = $("#gallery-cell-template").clone();
-  gallery_cell_height = gallery_cell_template.height();
-  console.log($("#gallery-cell-template").height())
-  //galleryInitial();
-  galleryInitial2();
+  gallery_cell_height = $("#gallery-cell-template").height();
+  galleryInitial();
 });//------------------------------------------------------------------------
 
 function category(category) {
   cur_category = category;
   filterCategory()
 }
-function galleryInitial2() {
+function galleryInitial() {
   gallery_div.children().remove()
   let gallery_videos_len = gallery_videos.length
   for (let i = 0; i < gallery_videos_len; i++) {         
     let cur_gallery_cell = gallery_cell_template.clone();
-    console.log(cur_gallery_cell)
-    cur_gallery_cell.css({
-        "left": (20 + 4) * (i % 4) + 4 + "%",
-        "top": (gallery_cell_height + 18) * Math.floor(i / 4)
-    });
+    // Position
+    if (window.matchMedia("(min-width: 992px)").matches){
+      cur_gallery_cell.css({
+          "left": (20 + 4) * (i % 4) + 4 + "%",
+          "top": (gallery_cell_height + 18) * Math.floor(i / 4)
+      });
+    }
+    else{
+      cur_gallery_cell.css({
+          "left": "4%",
+          "top": (gallery_cell_height + 18) * i
+      });
+    }
+    // Get Metadata
+    let cell_video_id = gallery_videos[i]["ID"];
+    let cell_category = gallery_videos[i]["category"];
+    let cell_category_class = category2class(cell_category)
     // Whole Block Attribute
     cur_gallery_cell.attr("id", "gallery_cell_" + i);
+    cur_gallery_cell.attr("title", gallery_videos[i]["title"]);
+    cur_gallery_cell.attr("category_class", cell_category_class)
 
     cur_gallery_cell.appendTo(gallery_div);
   }
-  gallery_div.children().each(function(index) {
-    console.log($(this))
-  });
 }
+/*
 function galleryInitial(){
   gallery_row.children().remove();
   let gallery_videos_len = gallery_videos.length
@@ -90,10 +94,10 @@ function galleryInitial(){
     col_cur.find(".video-title").text(gallery_videos[i]["title"]);
     col_cur.find(".video-date").text(gallery_videos[i]["year"] + "-" + gallery_videos[i]["month"] + "-" + gallery_videos[i]["day"]);
     col_cur.find(".video-category").text(video_category);
-    col_cur.appendTo(gallery_row);
-    
+    col_cur.appendTo(gallery_row);    
   }
 }
+*/
 function checkDuplicateID() {
   // Warning Duplicate IDs
   $('[id]').each(function() {
