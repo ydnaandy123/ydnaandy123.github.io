42a0a23cce861fe9b03b39699ea8830e3e367dd2
Jui-Ting Chien <ydnaandy123@gmail.com>
2018-08-09 10:33:28 +0800

    100% width

--- other/xinhu/javascript/gallery.js
+++ other/xinhu/javascript/gallery.js
@@ -2,34 +2,46 @@ var gallery_div, gallery_cell_template, gallery_cell_height, cur_class="all";
 
 function filterClass(category_class) {
   cur_class = category_class
-  if (cur_class == "all"){
+  let existed_index = 0;
     gallery_div.children().each(function(index) {
-      console.log(index);
+      if ($(this).attr("category_class") != cur_class  && cur_class != "all") {
         $(this).css({
-            "opacity": "1.0"
-        })
-        //$(this).show()
-    });
-  }
-  else{
-    gallery_div.children().each(function(index) {
-        if ($(this).attr("category_class") != cur_class) {
+            "opacity": "0.0"
+        });
+        $(this).hide()
+      }
+      else{
+        $(this).show()
+        // Position
+        if (window.matchMedia("(min-width: 992px)").matches){
           $(this).css({
-              "opacity": "0.0"
+              "opacity": "1.0",
+              "left": (24 + 1) * (existed_index % 4) + 0.5 + "%",
+              "top": (gallery_cell_height + 18) * Math.floor(existed_index / 4)
           });
-          //$(this).hide()
         }
         else{
           $(this).css({
               "opacity": "1.0",
-          })
-          //$(this).show()
+              "top": (gallery_cell_height + 18) * existed_index
+          });
         }
+        existed_index++;
+      }
     });
-  }
   gallery_div.removeClass(gallery_div.attr("cur_class"))
   gallery_div.attr("cur_class", cur_class)
   gallery_div.addClass(cur_class)
+  if (window.matchMedia("(min-width: 992px)").matches){
+    gallery_div.css({
+        "height": (gallery_cell_height + 18) * Math.ceil(existed_index / 4)
+    })
+  }
+  else{
+    gallery_div.css({
+        "height": (gallery_cell_height + 18) * existed_index
+    })
+  }
 }
 //------------------------------------------------------------------------
 function category(category) {
@@ -44,13 +56,12 @@ function galleryInitial() {
     // Position
     if (window.matchMedia("(min-width: 992px)").matches){
       cur_gallery_cell.css({
-          "left": (20 + 4) * (i % 4) + 4 + "%",
+          "left": (24 + 1) * (i % 4) + 0.5 + "%",
           "top": (gallery_cell_height + 18) * Math.floor(i / 4)
       });
     }
     else{
       cur_gallery_cell.css({
-          "left": "4%",
           "top": (gallery_cell_height + 18) * i
       });
     }
@@ -62,8 +73,25 @@ function galleryInitial() {
     cur_gallery_cell.attr("id", "gallery_cell_" + i);
     cur_gallery_cell.attr("title", gallery_videos[i]["title"]);
     cur_gallery_cell.attr("category_class", cell_category_class)
+    // Image
+    cur_gallery_cell.find("a").attr("href", "http://www.youtube.com/watch?v=" + cell_video_id);
+    cur_gallery_cell.find("img").attr("src", "https://img.youtube.com/vi/" + cell_video_id + "/mqdefault.jpg");
+    // Description
+    cur_gallery_cell.find(".cell-title").text(gallery_videos[i]["title"]);
+    cur_gallery_cell.find(".cell-date").text(gallery_videos[i]["year"] + "-" + gallery_videos[i]["month"] + "-" + gallery_videos[i]["day"]);
+    cur_gallery_cell.find(".cell-category > button").text(cell_category).addClass(cell_category_class);    
 
     cur_gallery_cell.appendTo(gallery_div);
+  }
+  if (window.matchMedia("(min-width: 992px)").matches){
+    gallery_div.css({
+        "height": (gallery_cell_height + 18) * Math.ceil(gallery_videos_len / 4)
+    })
+  }
+  else{
+    gallery_div.css({
+        "height": (gallery_cell_height + 18) * gallery_videos_len
+    })
   }
 }
 /*
@@ -116,10 +144,22 @@ function animatedObjectCheck(){
 $(document).ready(function (e) {
   // Debug
   checkDuplicateID();
+  // Smooth Scroll
+  $("footer a[href='#top-div']").on('click', function (event) {
+    console.log(this.hash)
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;$('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function () {
+        window.location.hash = hash;
+      });
+    }
+  });
 });
 $(window).on('load', function (e) {
   // Get elements after document ready.
-  gallery_div = $("#gallery-div2");
+  gallery_div = $("#gallery-div");
   gallery_cell_template = $("#gallery-cell-template").clone();
   gallery_cell_height = $("#gallery-cell-template").height();
   galleryInitial();
