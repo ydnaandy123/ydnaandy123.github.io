101b7452b91908cd9b5276f393df031fd916cdd3
Jui-Ting Chien <ydnaandy123@gmail.com>
2018-09-12 12:25:11 +0800

    news update

--- other/xinhu/javascript/gallery_videos.js
+++ other/xinhu/javascript/gallery_videos.js
@@ -1,309 +1,45 @@
 ﻿var gallery_videos = [
-  {
-    "title": "新湖的一天 資源回收日",
-    "ID": "rFepsCb_6-c",
-    "month": 9,
-    "day": 15,
-    "year": 2017,
-    "category": "其他"
-  },
-  {
-    "title": "20170812 空拍新湖",
-    "ID": "sqJFw0JcSig",
-    "month": 9,
-    "day": 15,
-    "year": 2017,
-    "category": "其他"
-  },
-  {
-    "title": "防震宣導影片",
-    "ID": "mEnDc2qBers",
-    "month": 9,
-    "day": 15,
-    "year": 2017,
-    "category": "教育"
-  },
-  {
-    "title": "新湖的一天 兒童朝會",
-    "ID": "R5ID2WP0VF8",
-    "month": 9,
-    "day": 16,
-    "year": 2017,
-    "category": "其他"
-  },
-  {
-    "title": "106教師節大會",
-    "ID": "B5qHAzDOu2M",
-    "month": 9,
-    "day": 29,
-    "year": 2017,
-    "category": "活動"
-  },
-  {
-    "title": "田徑隊 -106秋季公開賽",
-    "ID": "uWmKyy9BIVA",
-    "month": 10,
-    "day": 4,
-    "year": 2017,
-    "category": "體育"
-  },
-  {
-    "title": "2017 中正盃滾球賽",
-    "ID": "UuZKstGAzf8",
-    "month": 11,
-    "day": 19,
-    "year": 2017,
-    "category": "體育"
-  },
-  {
-    "title": "106藝術亮點成果短片",
-    "ID": "olF06ZdDhHM",
-    "month": 11,
-    "day": 19,
-    "year": 2017,
-    "category": "美術"
-  },
-  {
-    "title": "106新湖體表會CF",
-    "ID": "uM1OAURvuvc",
-    "month": 11,
-    "day": 21,
-    "year": 2017,
-    "category": "體育"
-  },
-  {
-    "title": "【新湖的一天】宣導 ～ 走廊不奔跑",
-    "ID": "BO-LFJlpXyM",
-    "month": 12,
-    "day": 4,
-    "year": 2017,
-    "category": "教育"
-  },
-  {
-    "title": "中興國小來訪",
-    "ID": "f2BaX5FilzM",
-    "month": 12,
-    "day": 8,
-    "year": 2017,
-    "category": "活動"
-  },
-  {
-    "title": "106合唱團晨光音樂劇",
-    "ID": "gYnt_BnW4BI",
-    "month": 1,
-    "day": 3,
-    "year": 2018,
-    "category": "音樂"
-  },
-  {
-    "title": "腳踏車體驗課程",
-    "ID": "PS-qMbFyTPQ",
-    "month": 1,
-    "day": 21,
-    "year": 2018,
-    "category": "教育"
-  },
-  {
-    "title": "2018-三對三鬥牛賽",
-    "ID": "suRMQoa6C4c",
-    "month": 1,
-    "day": 21,
-    "year": 2018,
-    "category": "體育"
-  },
-  {
-    "title": "2018 卡資堡活動花絮",
-    "ID": "BVTEsmflgCE",
-    "month": 2,
-    "day": 2,
-    "year": 2018,
-    "category": "其他"
-  },
-  {
-    "title": "英雄任務",
-    "ID": "_rI0geDlvXo",
-    "month": 3,
-    "day": 2,
-    "year": 2018,
-    "category": "教育"
-  },
-  {
-    "title": "先鋒計畫",
-    "ID": "cF5Q0TglbG0",
-    "month": 3,
-    "day": 2,
-    "year": 2018,
-    "category": "教育"
-  },
-  {
-    "title": "107家長日報告",
-    "ID": "W-iPX1yr2kY",
-    "month": 3,
-    "day": 2,
-    "year": 2018,
-    "category": "教育"
-  },
-  {
-    "title": "享受剎那間的悸動",
-    "ID": "RJ-zqYifzuM",
-    "month": 3,
-    "day": 11,
-    "year": 2018,
-    "category": "音樂"
-  },
-  {
-    "title": "107學校日影片 我們不一樣",
-    "ID": "tGExIPu7xak",
-    "month": 3,
-    "day": 12,
-    "year": 2018,
-    "category": "其他"
-  },
-  {
-    "title": "新湖幸福頻道 - 口腔保健篇",
-    "ID": "RzS_mVCWQd8",
-    "month": 4,
-    "day": 8,
-    "year": 2018,
-    "category": "教育"
-  },
-  {
-    "title": "畢旅晚會",
-    "ID": "4TWZFidVp3Q",
-    "month": 4,
-    "day": 19,
-    "year": 2018,
-    "category": "活動"
-  },
-  {
-    "title": "優質短片final",
-    "ID": "ISAtVDm0zuM",
-    "month": 4,
-    "day": 29,
-    "year": 2018,
-    "category": "教育"
-  },
-  {
-    "title": "創新實驗 2分鐘版",
-    "ID": "eM5_q3rSmdM",
-    "month": 5,
-    "day": 7,
-    "year": 2018,
-    "category": "教育"
-  },
-  {
-    "title": "三個感動 完整版(字幕版）",
-    "ID": "JTUg0uKcLMw",
-    "month": 5,
-    "day": 7,
-    "year": 2018,
-    "category": "其他"
-  },
-  {
-    "title": "2018管樂團公演",
-    "ID": "JrLLHHXOcJo",
-    "month": 6,
-    "day": 1,
-    "year": 2018,
-    "category": "音樂"
-  },
-  {
-    "title": "107下合唱團晨光音樂劇",
-    "ID": "q4I7cpPdUhA",
-    "month": 6,
-    "day": 9,
-    "year": 2018,
-    "category": "音樂"
-  },
-  {
-    "title": "畢業生致贈母校紀念品",
-    "ID": "JJBNu52_Ofw",
-    "month": 6,
-    "day": 18,
-    "year": 2018,
-    "category": "其他"
-  },
-  {
-    "title": "新湖國小畢業影片-正式版",
-    "ID": "AI1s-B5AxYU",
-    "month": 6,
-    "day": 20,
-    "year": 2018,
-    "category": "活動"
-  },
-  {
-    "title": "校園巡禮 新湖國小",
-    "ID": "gfS63TXSB2o",
-    "month": 6,
-    "day": 23,
-    "year": 2018,
-    "category": "活動"
-  },
-  {
-    "title": "偉大的渺小　月香主任留念",
-    "ID": "PHA6VvuDMcI",
-    "month": 7,
-    "day": 2,
-    "year": 2018,
-    "category": "其他"
-  },
-  {
-    "title": "106學年度校際交流",
-    "ID": "KPaPATF2q-4",
-    "month": 7,
-    "day": 8,
-    "year": 2018,
-    "category": "活動"
-  },
-  {
-    "title": "1070806新湖國小暑期攀岩活動",
-    "ID": "BJ_saYk8J0A",
-    "month": 8,
-    "day": 16,
-    "year": 2018,
-    "category": "活動"
-  },
-  {
-    "title": "1070830新湖國小新生迎新活動",
-    "ID": "hKzwLvQ8GS4",
-    "month": 8,
-    "day": 30,
-    "year": 2018,
-    "category": "活動"
-  },
-  {
-    "title": "20180607小田園工作日誌",
-    "ID": "TaXQihY23SQ",
-    "month": 6,
-    "day": 7,
-    "year": 2018,
-    "category": "教育"
-  },
-  {
-    "title": "20180531小田園工日誌",
-    "ID": "cxFvnJazzZQ",
-    "month": 5,
-    "day": 31,
-    "year": 2018,
-    "category": "教育"
-  },
-  {
-    "title": "20180507圖書館工作日誌",
-    "ID": "8Tb5P178KGY",
-    "month": 5,
-    "day": 7,
-    "year": 2018,
-    "category": "其他"
-  },
-  {
-    "title": "20180831警察杯杯在新湖",
-    "ID": "X09MspQI8JQ",
-    "month": 31,
-    "day": 8,
-    "year": 2018,
-    "category": "教育"
-  },{"title":"20140619海洋音樂祭創作花絮","ID":"_a47DBjOEHg","month":6.00,"day":19.00,"year":2014.00,"category":"美術"},
-  {"title":"20170621藝童向前行 幸福藍晒圖","ID":"8dJcnzT39Uc","month":6.00,"day":21.00,"year":2017.00,"category":"美術"},
-  {"title":"20160620貓熊快閃在新湖","ID":"GSJHd_5QaNQ","month":6.00,"day":20.00,"year":2016.00,"category":"美術"},
-  {"title":"20160414幸福雪花祭","ID":"9eajoShw9k0","month":4.00,"day":14.00,"year":2016.00,"category":"美術"}
- ];
+{"title":"新湖的一天 資源回收日","ID":"rFepsCb_6-c","month":9.00,"day":15.00,"year":2017.00,"category":"其他"},
+{"title":"20170812 空拍新湖","ID":"sqJFw0JcSig","month":9.00,"day":15.00,"year":2017.00,"category":"其他"},
+{"title":"防震宣導影片","ID":"mEnDc2qBers","month":9.00,"day":15.00,"year":2017.00,"category":"教育"},
+{"title":"新湖的一天 兒童朝會","ID":"R5ID2WP0VF8","month":9.00,"day":16.00,"year":2017.00,"category":"其他"},
+{"title":"106教師節大會","ID":"B5qHAzDOu2M","month":9.00,"day":29.00,"year":2017.00,"category":"活動"},
+{"title":"田徑隊 -106秋季公開賽","ID":"uWmKyy9BIVA","month":10.00,"day":4.00,"year":2017.00,"category":"體育"},
+{"title":"2017 中正盃滾球賽","ID":"UuZKstGAzf8","month":11.00,"day":19.00,"year":2017.00,"category":"體育"},
+{"title":"106藝術亮點成果短片","ID":"olF06ZdDhHM","month":11.00,"day":19.00,"year":2017.00,"category":"美術"},
+{"title":"106新湖體表會CF","ID":"uM1OAURvuvc","month":11.00,"day":21.00,"year":2017.00,"category":"體育"},
+{"title":"【新湖的一天】宣導 ～ 走廊不奔跑","ID":"BO-LFJlpXyM","month":12.00,"day":4.00,"year":2017.00,"category":"教育"},
+{"title":"中興國小來訪","ID":"f2BaX5FilzM","month":12.00,"day":8.00,"year":2017.00,"category":"活動"},
+{"title":"106合唱團晨光音樂劇","ID":"gYnt_BnW4BI","month":1.00,"day":3.00,"year":2018.00,"category":"音樂"},
+{"title":"腳踏車體驗課程","ID":"PS-qMbFyTPQ","month":1.00,"day":21.00,"year":2018.00,"category":"教育"},
+{"title":"2018-三對三鬥牛賽","ID":"suRMQoa6C4c","month":1.00,"day":21.00,"year":2018.00,"category":"體育"},
+{"title":"2018 卡資堡活動花絮","ID":"BVTEsmflgCE","month":2.00,"day":2.00,"year":2018.00,"category":"其他"},
+{"title":"英雄任務","ID":"_rI0geDlvXo","month":3.00,"day":2.00,"year":2018.00,"category":"教育"},
+{"title":"先鋒計畫","ID":"cF5Q0TglbG0","month":3.00,"day":2.00,"year":2018.00,"category":"教育"},
+{"title":"107家長日報告","ID":"W-iPX1yr2kY","month":3.00,"day":2.00,"year":2018.00,"category":"教育"},
+{"title":"享受剎那間的悸動","ID":"RJ-zqYifzuM","month":3.00,"day":11.00,"year":2018.00,"category":"音樂"},
+{"title":"107學校日影片 我們不一樣","ID":"tGExIPu7xak","month":3.00,"day":12.00,"year":2018.00,"category":"其他"},
+{"title":"新湖幸福頻道 - 口腔保健篇","ID":"RzS_mVCWQd8","month":4.00,"day":8.00,"year":2018.00,"category":"教育"},
+{"title":"畢旅晚會","ID":"4TWZFidVp3Q","month":4.00,"day":19.00,"year":2018.00,"category":"活動"},
+{"title":"優質短片final","ID":"ISAtVDm0zuM","month":4.00,"day":29.00,"year":2018.00,"category":"教育"},
+{"title":"創新實驗 2分鐘版","ID":"eM5_q3rSmdM","month":5.00,"day":7.00,"year":2018.00,"category":"教育"},
+{"title":"三個感動 完整版(字幕版）","ID":"JTUg0uKcLMw","month":5.00,"day":7.00,"year":2018.00,"category":"其他"},
+{"title":"2018管樂團公演","ID":"JrLLHHXOcJo","month":6.00,"day":1.00,"year":2018.00,"category":"音樂"},
+{"title":"107下合唱團晨光音樂劇","ID":"q4I7cpPdUhA","month":6.00,"day":9.00,"year":2018.00,"category":"音樂"},
+{"title":"畢業生致贈母校紀念品","ID":"JJBNu52_Ofw","month":6.00,"day":18.00,"year":2018.00,"category":"其他"},
+{"title":"新湖國小畢業影片-正式版","ID":"AI1s-B5AxYU","month":6.00,"day":20.00,"year":2018.00,"category":"活動"},
+{"title":"校園巡禮 新湖國小","ID":"gfS63TXSB2o","month":6.00,"day":23.00,"year":2018.00,"category":"活動"},
+{"title":"偉大的渺小　月香主任留念","ID":"PHA6VvuDMcI","month":7.00,"day":2.00,"year":2018.00,"category":"其他"},
+{"title":"106學年度校際交流","ID":"KPaPATF2q-4","month":7.00,"day":8.00,"year":2018.00,"category":"活動"},
+{"title":"1070806新湖國小暑期攀岩活動","ID":"BJ_saYk8J0A","month":8.00,"day":16.00,"year":2018.00,"category":"活動"},
+{"title":"1070830新湖國小新生迎新活動","ID":"hKzwLvQ8GS4","month":8.00,"day":30.00,"year":2018.00,"category":"活動"},
+{"title":"20180607小田園工作日誌","ID":"TaXQihY23SQ","month":8.00,"day":6.00,"year":2018.00,"category":"教育"},
+{"title":"20180531小田園工作日誌","ID":"n4pJlBRn0xU","month":5.00,"day":31.00,"year":2018.00,"category":"教育"},
+{"title":"201805067圖書館工作日誌","ID":"1IV6UTDOG2U","month":5.00,"day":7.00,"year":2018.00,"category":"其他"},
+{"title":"20180831警察杯杯在新湖","ID":"X09MspQI8JQ","month":31.00,"day":8.00,"year":2018.00,"category":"教育"},
+{"title":"20140619海洋音樂祭創作花絮","ID":"_a47DBjOEHg","month":6.00,"day":19.00,"year":2014.00,"category":"美術"},
+{"title":"20170621藝童向前行 幸福藍晒圖","ID":"8dJcnzT39Uc","month":6.00,"day":21.00,"year":2017.00,"category":"美術"},
+{"title":"20160620貓熊快閃在新湖","ID":"GSJHd_5QaNQ","month":6.00,"day":20.00,"year":2016.00,"category":"美術"},
+{"title":"20160414幸福雪花祭","ID":"9eajoShw9k0","month":4.00,"day":14.00,"year":2016.00,"category":"美術"},
+{"title":"20160618回收再利用 快閃黑白紙熊貓變彩色 中視新聞","ID":"UHeLJyqj0iU","month":6.00,"day":18.00,"year":2016.00,"category":"美術"}
+];
