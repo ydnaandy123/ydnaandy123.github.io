a2cf20a48c020ea449b14ef74e436f157eb79777
Jui-Ting Chien <ydnaandy123@gmail.com>
2018-09-19 16:14:59 +0800

    banner

--- other/xinhu/backup_new/gallery.js
+++ other/xinhu/backup_new/gallery.js
@@ -1,5 +1,52 @@
-var sorting_div, sorting_btn, gallery_div, gallery_div_row, gallery_cell_template, cur_class="classColorAll";
+var sorting_div, sorting_btn, gallery_div, gallery_cell_template, gallery_cell_height, cur_class="classColorAll";
 
+function filterClassSort() {
+  let existed_index = 0;
+    gallery_div.children().each(function(index) {
+      if ($(this).attr("category_class") != cur_class  && cur_class != "classColorAll") {
+        $(this).css({
+            "opacity": "0.0"
+        });
+        $(this).hide()
+      }
+      else{
+        $(this).show()
+        // Position
+        if (window.matchMedia("(min-width: 992px)").matches){
+          $(this).css({
+              "opacity": "1.0",
+              "left": (24 + 1) * (existed_index % 4) + 0.5 + "%",
+              "top": (gallery_cell_height + 18) * Math.floor(existed_index / 4)
+          });
+        }
+        else{
+          $(this).css({
+              "opacity": "1.0",
+              "top": (gallery_cell_height + 18) * existed_index
+          });
+        }
+        existed_index++;
+      }
+    });
+  gallery_div.removeClass(gallery_div.attr("data-cur-class"))
+  gallery_div.addClass(cur_class)
+  sorting_div.removeClass(gallery_div.attr("data-cur-class"))
+  sorting_div.addClass(cur_class)
+  sorting_btn.removeClass(gallery_div.attr("data-cur-class"))
+  sorting_btn.addClass(cur_class)
+  gallery_div.attr("data-cur-class", cur_class)
+  if (window.matchMedia("(min-width: 992px)").matches){
+    gallery_div.css({
+        "height": (gallery_cell_height + 18) * Math.ceil(existed_index / 4)
+    })
+  }
+  else{
+    gallery_div.css({
+        "height": (gallery_cell_height + 18) * existed_index
+    })
+  }
+}
+//------------------------------------------------------------------------
 function sortGallery(sortOrder) {
   if (sortOrder == "newest") {
       gallery_videos.sort(function(b, a) {
@@ -13,79 +60,42 @@ function sortGallery(sortOrder) {
   galleryInitial();
   filterClassSort();
 }
-function topCoverChange(){
-  $("#top-div img").css({
-    "opacity": "0.0"
-  });
-  if (cur_class=="classColorAll"){
-    $("#top-img0").css({
-      "opacity": "1.0"
-    });
-  }
-  else if(cur_class=="classColor1"){
-    $("#top-img1").css({
-      "opacity": "1.0"
-    });
-  }
-  else if(cur_class=="classColor2"){
-    $("#top-img2").css({
-      "opacity": "1.0"
-    });
-  }
-  else if(cur_class=="classColor3"){
-    $("#top-img3").css({
-      "opacity": "1.0"
-    });
-  }
-  else if(cur_class=="classColor4"){
-    $("#top-img4").css({
-      "opacity": "1.0"
-    });
-  }
-  else if(cur_class=="classColor5"){
-    $("#top-img5").css({
-      "opacity": "1.0"
-    });
-  }
-  else if(cur_class=="classColor6"){
-    $("#top-img6").css({
-      "opacity": "1.0"
-    });
+function category2class(category) {
+  if (category == "活動") {
+      return "classColor1"
+  } else if (category == "體育") {
+      return "classColor2"
+  } else if (category == "音樂") {
+      return "classColor3"
+  } else if (category == "美術") {
+      return "classColor4"
+  } else if (category == "教育") {
+      return "classColor5"
+  } else if (category == "其他") {
+      return "classColor6"
   }
 }
 function filterClass(category_class) {
   cur_class = category_class
-  topCoverChange();
   filterClassSort();
 }
-function filterClassSort() {
-  gallery_div_row.children().each(function(index) {
-      if ($(this).attr("category_class") != cur_class  && cur_class != "classColorAll") {
-        $(this).css({
-          "display": "none"
-        });
-      }
-      else{
-        $(this).css({
-          "display": "block"
-        });
-      }
-    });
-  gallery_div_row.removeClass(gallery_div_row.attr("data-cur-class"))
-  gallery_div_row.addClass(cur_class)
-  sorting_div.removeClass(gallery_div_row.attr("data-cur-class"))
-  sorting_div.addClass(cur_class)
-  sorting_btn.removeClass(gallery_div_row.attr("data-cur-class"))
-  sorting_btn.addClass(cur_class)
-  gallery_div.removeClass(gallery_div_row.attr("data-cur-class"))
-  gallery_div.addClass(cur_class)
-  gallery_div_row.attr("data-cur-class", cur_class)
-}
 function galleryInitial() {
-  gallery_div_row.children().remove()
+  gallery_div.children().remove()
   let gallery_videos_len = gallery_videos.length
   for (let i = 0; i < gallery_videos_len; i++) {         
     let cur_gallery_cell = gallery_cell_template.clone();
+    // Position
+    if (window.matchMedia("(min-width: 992px)").matches){
+      cur_gallery_cell.css({
+          "left": (24 + 1) * (i % 4) + 0.5 + "%",
+          "top": (gallery_cell_height + 18) * Math.floor(i / 4)
+      });
+    }
+    else{
+      cur_gallery_cell.css({
+          "top": (gallery_cell_height + 18) * i
+      });
+    }
     // Get Metadata
     let cell_video_id = gallery_videos[i]["ID"];
     let cell_video_url = "http://www.youtube.com/watch?v=" + cell_video_id
@@ -100,11 +110,20 @@ function galleryInitial() {
     cur_gallery_cell.find("img").attr("src", "https://img.youtube.com/vi/" + cell_video_id + "/mqdefault.jpg");
     // Description
     cur_gallery_cell.find(".cell-title").attr("href", cell_video_url).find("h6").text(gallery_videos[i]["title"]);
-    let date_string = gallery_videos[i]["year"] + "-" + gallery_videos[i]["month"] + "-" + gallery_videos[i]["day"];
-    cur_gallery_cell.find(".cell-date").text(date_string).attr("title", date_string);
+    cur_gallery_cell.find(".cell-date").text(gallery_videos[i]["year"] + "-" + gallery_videos[i]["month"] + "-" + gallery_videos[i]["day"]);
     cur_gallery_cell.find(".cell-category > button").text(cell_category).addClass(cell_category_class).attr("title", cell_category).attr('onClick',"filterClass('"+ cell_category_class +"')");        
 
-    cur_gallery_cell.appendTo(gallery_div_row);
+    cur_gallery_cell.appendTo(gallery_div);
+  }
+  if (window.matchMedia("(min-width: 992px)").matches){
+    gallery_div.css({
+        "height": (gallery_cell_height + 18) * Math.ceil(gallery_videos_len / 4)
+    })
+  }
+  else{
+    gallery_div.css({
+        "height": (gallery_cell_height + 18) * gallery_videos_len
+    })
   }
 }
 function checkDuplicateID() {
@@ -155,24 +174,9 @@ $(window).on('load', function (e) {
   // Get elements after document ready.
   sorting_div = $("#sorting-div");
   sorting_btn = $("#sorting-btn");
-  gallery_div_row = $("#gallery-div .container .row");
   gallery_div = $("#gallery-div");
   gallery_cell_template = $("#gallery-cell-template").clone();
+  gallery_cell_height = $("#gallery-cell-template").height();
   galleryInitial();
   animatedObjectCheck();
 })
-function category2class(category) {
-  if (category == "活動") {
-      return "classColor1"
-  } else if (category == "體育") {
-      return "classColor2"
-  } else if (category == "音樂") {
-      return "classColor3"
-  } else if (category == "美術") {
-      return "classColor4"
-  } else if (category == "教育") {
-      return "classColor5"
-  } else if (category == "其他") {
-      return "classColor6"
-  }
-}
